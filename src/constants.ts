f489fa805f6ab3500d4de869fcbb4f19f1000abb
mayank2130 <mayankthakur1712@gmail.com>
2024-02-26 13:47:28 +0530

    first commit

---
+++ src/constants.ts
@@ -0,0 +1,75 @@
+export const currencyByRupee: Currency[] = [
+    {
+      name: 'DOLLAR',
+      value: 0.012271428,
+      flag: '🇺🇸',
+      symbol: '$',
+    },
+    {
+      name: 'EURO',
+      value: 0.01125809,
+      flag: '🇪🇺',
+      symbol: '€',
+    },
+    {
+      name: 'POUND',
+      value: 0.0095,
+      flag: '🇬🇧',
+      symbol: '£',
+    },
+    {
+      name: 'RUBEL',
+      value: 1.12,
+      flag: '🇷🇺',
+      symbol: '₽',
+    },
+    {
+      name: 'AUS DOLLAR',
+      value: 0.018,
+      flag: '🇦🇺',
+      symbol: 'A$',
+    },
+    {
+      name: 'CAN DOLLAR',
+      value: 0.016457908,
+      flag: '🇨🇦',
+      symbol: 'C$',
+    },
+    {
+      name: 'YEN',
+      value: 1.81,
+      flag: '🇯🇵',
+      symbol: '¥',
+    },
+    {
+      name: 'DINAR',
+      value: 0.0037446993,
+      flag: '🇰🇼',
+      symbol: 'KD',
+    },
+    {
+      name: 'BITCOIN',
+      value: 0.0000002345,
+      flag: '🎰',
+      symbol: '₿',
+    },
+    {
+      name: 'SWISS FRANC',
+      value: 0.0115,
+      flag: '🇨🇭',
+      symbol: 'CHF',
+    },
+    {
+      name: 'SWEDISH KRONA',
+      value: 0.124,
+      flag: '🇸🇪',
+      symbol: 'SEK',
+    },
+    {
+      name: 'SINGAPORE DOLLAR',
+      value: 0.017,
+      flag: '🇸🇬',
+      symbol: 'S$',
+    },
+  ];
+  
